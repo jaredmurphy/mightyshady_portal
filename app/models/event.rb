e86e91e87eabb0ec48a6a2c18ae72e38238cfcb6
Jared <jaredcmurphy@gmail.com>
2016-11-14 00:15:14 -0500

    add booking models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,11 @@
 class Event < ApplicationRecord
+#   belongs_to :venue,
+#   class_name: "Venue",
+#   foreign_key: "venue_id"
+#
+#   belongs_to :event_category,
+#     class_name: "Event Category",
+#     foreign_key: "event_category_id"
+#
+# scope :with_results, -> { includes(:venue, :event_category_id) }
 end
